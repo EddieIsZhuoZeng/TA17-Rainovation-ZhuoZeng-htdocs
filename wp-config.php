9be9a29d47f7c020ddbcb850cf0d433d45b9a328
Zhuo Zeng <49259295+EddieIsZhuoZeng@users.noreply.github.com>
2020-11-03 07:47:17 +0800

    I3-uploaded-by-eddie

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define( 'DB_NAME', 'bitnami_wordpress' );
 define( 'DB_USER', 'bn_wordpress' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'efaa480277' );
+define( 'DB_PASSWORD', '0438c49da5' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost:3306' );
@@ -46,14 +46,14 @@ define( 'DB_COLLATE', '' );
  *
  * @since 2.6.0
  */
-define('AUTH_KEY', '30c6821d827f0eec804d97f6366d990a697375ca4201515118d4a14f646c6001');
-define('SECURE_AUTH_KEY', '4c0d1a52bb69c69808d4e57b184ea3751b5df93dd7bf5172a5879c36229d178f');
-define('LOGGED_IN_KEY', '3ba5498d4b634fa1717e2d47d0e7b414a83a3cd834ba7b3ade93fec24de6f9d8');
-define('NONCE_KEY', '80514b74bc8bae87903537fd187ef9afaec02eddbd5a91ba46b90028a403f9e4');
-define('AUTH_SALT', 'd135a6f1fc5a7da751e20bf5cd261c6d087621f2873280e6898ef642eaeb83ff');
-define('SECURE_AUTH_SALT', '3da81de8d4e9bd3c7395e071b4218fa02ade4fc9e1f1dcfcfb4b239beca9b1ce');
-define('LOGGED_IN_SALT', '550204113a03645eca315cdfc47cfa5e20594dbf0d4598657929183f4a3377b2');
-define('NONCE_SALT', '5e5e83adff9b66770ad53434d572fd2fe702a575c4216740066ab816c1de63b1');
+define('AUTH_KEY', '2552c48edb0c75f0f8aed98c34d2e504a6038c279e0e46742373e8cbd70372c9');
+define('SECURE_AUTH_KEY', '912df110cd1baf51e275598dc978b3e6989e2a0cae5636b2662b0b8575d81dce');
+define('LOGGED_IN_KEY', '3a3ba9e2a2da3f8af86944e6b677eaee99714ee6d8f54302911a16797927113e');
+define('NONCE_KEY', '1eabc8475ae32e0970c06417032b274f986a410a096442a0f7c731c95518eda1');
+define('AUTH_SALT', 'd0e2281141687a7258aadbe5d5ad301d281e6a37c7c47d1f88f53b4730bb97f3');
+define('SECURE_AUTH_SALT', '3ddc6d75d5e59fd8d01c9a48eda3474b99544b20d97077dac1ae3a4000c9650c');
+define('LOGGED_IN_SALT', '16ce95fad7cd9cbfcd5fd2acdaf2765a0956c50bba51fab644f811d797e0800c');
+define('NONCE_SALT', 'e4df40ff9016cc9fe361aab131de85ac61f9b99565172c7a004262e4fe31b592');
 
 /**#@-*/
 
@@ -86,8 +86,8 @@ define('FS_METHOD', 'direct');
 /**
  * The WP_SITEURL and WP_HOME options are configured to access from any hostname or IP address.
  * If you want to access only from an specific domain, you can modify them. For example:
- *  define('WP_HOME','http://example.com');
- *  define('WP_SITEURL','http://example.com');
+ *  define('WP_HOME','https://example.com');
+ *  define('WP_SITEURL','https://example.com');
  *
 */
 
@@ -95,8 +95,8 @@ if ( defined( 'WP_CLI' ) ) {
     $_SERVER['HTTP_HOST'] = 'localhost';
 }
 
-define('WP_SITEURL', 'http://' . $_SERVER['HTTP_HOST'] . '/');
-define('WP_HOME', 'http://' . $_SERVER['HTTP_HOST'] . '/');
+define('WP_SITEURL','https://' . $_SERVER['HTTP_HOST'] . '/');
+define('WP_HOME','https://' . $_SERVER['HTTP_HOST'] . '/');
 
 
 /** Absolute path to the WordPress directory. */
@@ -126,3 +126,5 @@ if ( !defined( 'WP_CLI' ) ) {
     });
     add_filter( 'auto_update_translation', '__return_false' );
 }
+define( 'WP_HOME', 'https://altered-urban.ml' );
+define( 'WP_SITEURL', 'https://altered-urban.ml' );
